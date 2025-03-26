e3a0df2782915dd93a51fd1e6e5b7c1c65c792df
mika <printf.mika@gmail.com>
2025-03-26 04:49:52 +0300

    configure publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+import org.jreleaser.model.Active
 
 plugins {
     kotlin("multiplatform") version "2.1.10"
+    id("org.jreleaser") version "1.17.0"
     `maven-publish`
 }
 
-group = "my.utilities"
+group = "dev.mimimishkin"
 version = "1.0.1"
 
 kotlin {
@@ -20,8 +22,38 @@ kotlin {
     }
 }
 
-publishing {
-    repositories {
-        mavenCentral()
+jreleaser {
+    project {
+        inceptionYear = "2025"
+        authors = listOf("mimimishkin")
+    }
+
+    release {
+        github {
+            skipRelease = true
+            skipTag = true
+        }
+    }
+
+    signing {
+        active = Active.ALWAYS
+        armored = true
+        verify = true
+    }
+
+    deploy {
+        maven {
+            mavenCentral.create("sonatype") {
+                active = Active.ALWAYS
+                url = "https://central.sonatype.com/api/v1/publisher"
+                stagingRepositories = listOf(layout.buildDirectory.dir("staging-deploy").get().toString())
+                setAuthorization("Basic")
+                sign = true
+                checksums = true
+                sourceJar = true
+                javadocJar = true
+                retryDelay = 60
+            }
+        }
     }
 }
