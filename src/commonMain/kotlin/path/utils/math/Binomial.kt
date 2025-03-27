bf90dc0a3ae21adcd5917edd6abcbde98ea00b62
mika <printf.mika@gmail.com>
2025-03-27 04:17:06 +0300

    remove indents

--- src/commonMain/kotlin/path/utils/math/Binomial.kt
+++ src/commonMain/kotlin/path/utils/math/Binomial.kt
@@ -1,13 +1,13 @@
 package path.utils.math
 
 private val lut = mutableListOf<List<Double>>(
-             listOf(1.0),           // n=0
-            listOf(1.0,1.0),          // n=1
-           listOf(1.0,2.0,1.0),         // n=2
-          listOf(1.0,3.0,3.0,1.0),        // n=3
-         listOf(1.0,4.0,6.0,4.0,1.0),       // n=4
-        listOf(1.0,5.0,10.0,10.0,5.0,1.0),    // n=5
-       listOf(1.0,6.0,15.0,20.0,15.0,6.0,1.0)   // n=6
+    listOf(1.0),           // n=0
+    listOf(1.0,1.0),          // n=1
+    listOf(1.0,2.0,1.0),         // n=2
+    listOf(1.0,3.0,3.0,1.0),        // n=3
+    listOf(1.0,4.0,6.0,4.0,1.0),       // n=4
+    listOf(1.0,5.0,10.0,10.0,5.0,1.0),    // n=5
+    listOf(1.0,6.0,15.0,20.0,15.0,6.0,1.0)   // n=6
 )
 private val nul = listOf(0.0)
 
